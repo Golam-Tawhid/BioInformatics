89e62f5af27e806c182e8883e3329ab658baa19b
Md Golam Tawhid Fahad <107804579+Golam-Tawhid@users.noreply.github.com>
2023-06-15 15:10:13 +0000

    Task 2 fixed

--- Task 2/task2.py
+++ Task 2/task2.py
@@ -1,38 +1,35 @@
 """
-This function returns the final sequence
+Solution of CS-centric task
 """
 
 
 def dna_sequence(arr):
     """
-    Find the matches and add them together
+    this function do the process of DNA sequence assembly.
     """
-    fin = ""
-
-    for i, dna1 in enumerate(arr):
-        for j, dna2 in enumerate(arr):
-            idx = -1
-            while idx >= -5:
-                prefix = dna1[idx:]
-                idx -= 1
-                if dna2.startswith(prefix) and i != j:
-                    fin += dna1
-                    fin += dna2.lstrip(prefix)
-                    break
-
-    return fin
+    final = arr[0]
+    for i in range(1, len(arr)):
+        match = find_match(final, arr[i])
+        final += arr[i][match:]
+    return final
 
 
-inpFile = open("Task 2/input2.txt", "r", encoding='utf-8')
-outFile = open("Task 2/output2.txt", "w", encoding='utf-8')
+def find_match(str1, str2):
+    """
+    this function find the match of two dna sequence.
+    """
+    max_len = min(len(str1), len(str2))
+    for i in range(max_len, 0, -1):
+        if str1[-i:] == str2[:i]:
+            return i
+
 
 inputs = []
 while True:
-    inp = inpFile.readline()
+    inp = input()
     print(inp)
     if inp == "":
         break
     inputs.append(inp)
 print(inputs)
-print(dna_sequence(inputs), file=outFile)
-outFile.close()
+print(dna_sequence(inputs))
